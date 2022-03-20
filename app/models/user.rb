285baf3cb1c9658b8f52e8334e07891a20dfc003
pnsandeepkumar <naga.sandeepkumar@freshworks.com>
2022-03-20 20:04:44 +0530

    added fn and ln to signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,7 @@ class User < ApplicationRecord
   end
 
   def full_name 
-    "#{first_name} #{last_name}"
+    return "#{first_name} #{last_name}" if first_name || last_name 
+    return "<Name Not Set>"
   end
 end
